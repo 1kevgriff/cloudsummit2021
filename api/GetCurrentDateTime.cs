1258395a24c44ab0cc43a6d24638370ed13e053b
Kevin Griffin <kevin@solliance.net>
2021-09-23 16:15:11 -0400

    add api

---
+++ api/GetCurrentDateTime.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Griffin
+{
+    public static class GetCurrentDateTime
+    {
+        [FunctionName("GetCurrentDateTime")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return new OkObjectResult(now);
+        }
+    }
+}
